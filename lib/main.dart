0ea746df9260b572d866c6b6db11ace7611348fb
Hyeon Lee <103764100+hyeonlee717@users.noreply.github.com>
2024-11-04 00:17:19 +0900

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const App());
+}
+
+class App extends StatelessWidget {
+  const App({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(home: HomePage());
+  }
+}
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Text('Hello, World!');
+  }
+}
